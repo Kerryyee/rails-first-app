4745d2b09a06ca98f4e022a9e3f195ec4bada234
kerry pang <kerrypang15@gmail.com>
2022-05-16 15:48:23 +0200

    Stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,13 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:input]
+    if @question == "I am going to work"
+      @answer = "Great!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
